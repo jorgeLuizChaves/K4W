a7b1154f833a0d3a0b66cea280f1d68c9a179478
Jorge Oliveira <jorge.luizchaves@yahoo.com.br>
2015-06-27 21:52:51 -0300

    hi world

--- K4WHi/MainWindow.xaml.cs
+++ K4WHi/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Kinect.Toolkit;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,38 @@ namespace K4WHi
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const string kinectStatus = "status";
+        private KinectSensorChooser _sensorChooser;
+
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += OnLoaded;
+        }
+
+        void OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _sensorChooser = new KinectSensorChooser();
+            _sensorChooser.KinectChanged += (o, args) =>
+            {
+                if (args.NewSensor != null)
+                {
+                    Debug.WriteLine("we have a new sensor");
+                }
+                else
+                {
+                    Debug.WriteLine("no Kinect sensor connected");
+                }
+            };
+
+            _sensorChooser.PropertyChanged += (o, args) => 
+            {
+                if (kinectStatus.Equals(args.PropertyName))
+                {
+                    Debug.WriteLine("Status = " + _sensorChooser.Status);
+                }
+                _sensorChooser.Start();
+            };
         }
     }
 }
